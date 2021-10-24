36e9136a45495f487816e3d1733b7092ec24154d
Rashid-F-Walcott <rashidwalcott@gmail.com>
2021-10-24 20:55:26 +0100

    "Finished Registration, Started FlightBookingService class and created a basic template for menu; needs methods to go inside"

--- src/main/java/com/FlightBooking/Main.java
+++ src/main/java/com/FlightBooking/Main.java
@@ -1,25 +1,29 @@
 package com.FlightBooking;
 
-import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        ListOfUsers.addUser();
+    public static void main(String[] args) throws InterruptedException {
+        ListOfUsers.addUserBase();
         ListOfUsers.displayListOfUsers();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to FlightBooker, please enter your email to log in");
         String emailCheck = scanner.nextLine();
 //        System.out.println("Please enter the unique user ID for the account belonging to " +emailCheck);
 //        int idCheck = scanner.nextInt();
-        if (ListOfUsers.checkExistingUser(emailCheck)){
-            System.out.println("Welcome back to FlightBooker!");
-        } else {
-            System.out.println("No account with those details could be found");
+        if (ListOfUsers.checkExistingUser(emailCheck)) {
         }
+        else {
+            ListOfUsers.addUser();
+        }
+        //  Next method to add is the 'menu' method with switch-case loop
+        FlightBookingService.FlightBookingMenu();
+
 
 
 
     }
 }
+
 
