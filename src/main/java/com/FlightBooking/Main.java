bd337c5f7b0347feccbb68665946caa1fd7092ca
jazim latif <jazimlatif@github.com>
2021-10-25 09:52:31 +0100

    importing classes

--- src/main/java/com/FlightBooking/Main.java
+++ src/main/java/com/FlightBooking/Main.java
@@ -1,5 +1,10 @@
 package com.FlightBooking;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<FlightDetails> flights = new ArrayList<>();
